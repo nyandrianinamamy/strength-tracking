4c4fa54b559c6f949343683982fba3858caff670
Mamy Razafintsialonina <nyandrianinamamy@gmail.com>
2026-04-02 21:57:16 +0200

    chore(debug): finish training engine debug screen verification

--- lib/src/app/router.dart
+++ lib/src/app/router.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:strength_training_tracker/src/core/app_state_controller.dart';
